8c96e739f0d12d8d31313d8cf088cbd2fc6f9344
박해민 <phm9368@sookmyung.ac.kr>
2021-09-24 14:44:20 +0900

    [Feat] Implement representatives list

--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeFragment.kt
@@ -1,70 +1,185 @@
 package com.example.android.politicalpreparedness.representative
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
-import android.view.*
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.AdapterView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.politicalpreparedness.BuildConfig
+import com.example.android.politicalpreparedness.R
 import com.example.android.politicalpreparedness.databinding.FragmentRepresentativeBinding
 import com.example.android.politicalpreparedness.network.models.Address
-import java.util.Locale
+import com.example.android.politicalpreparedness.representative.adapter.RepresentativeListAdapter
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
+import java.util.*
 
 class DetailFragment : Fragment() {
 
     companion object {
-        //TODO: Add Constant for Location request
+        private const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 
-    //TODO: Declare ViewModel
+    private lateinit var binding: FragmentRepresentativeBinding
+    private val viewModel: RepresentativeViewModel by lazy {
+        ViewModelProvider(this).get(RepresentativeViewModel::class.java)
+    }
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentRepresentativeBinding.inflate(inflater)
+        binding.lifecycleOwner = this
+        binding.vm = viewModel
 
-        //TODO: Establish bindings
-        val binding = FragmentRepresentativeBinding.inflate(inflater)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
-        //TODO: Define and assign Representative adapter
+        val adapter = RepresentativeListAdapter()
+        binding.representativesList.adapter = adapter
 
-        //TODO: Populate Representative adapter
+        viewModel.representatives.observe(viewLifecycleOwner, {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
 
-        //TODO: Establish button listeners for field and location search
+        binding.buttonSearch.setOnClickListener {
+            hideKeyboard()
+            viewModel.getRepresentatives()
+        }
+
+        binding.buttonLocation.setOnClickListener {
+            if (checkLocationPermissions()) {
+                getLocation()
+            } else {
+                requestLocationPermission()
+            }
+        }
+
+        binding.state.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                viewModel.address.value?.state = binding.state.selectedItem.toString()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                viewModel.address.value?.state = binding.state.selectedItem.toString()
+            }
+
+        }
 
         return binding.root
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        //TODO: Handle location permission result to get location on permission granted
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    getLocation()
+                } else {
+                    if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        Snackbar.make(
+                            binding.root,
+                            R.string.permission_denied_explanation,
+                            Snackbar.LENGTH_LONG
+                        ).setAction(R.string.settings) {
+                            startActivity(Intent().apply {
+                                action = ACTION_APPLICATION_DETAILS_SETTINGS
+                                data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            })
+                        }.show()
+                    } else {
+                        requestLocationPermission()
+                    }
+                }
+                return
+            }
+            else -> {
+            }
+        }
     }
 
     private fun checkLocationPermissions(): Boolean {
         return if (isPermissionGranted()) {
             true
         } else {
-            //TODO: Request Location permissions
+            requestLocationPermission()
             false
         }
     }
 
-    private fun isPermissionGranted() : Boolean {
-        //TODO: Check if permission is already granted and return (true = granted, false = denied/other)
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
+    @SuppressLint("MissingPermission")
     private fun getLocation() {
-        //TODO: Get location from LocationServices
-        //TODO: The geoCodeLocation method is a helper function to change the lat/long location to a human readable street address
+        if (isPermissionGranted()) {
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                location?.let {
+                    val address = geoCodeLocation(location)
+                    viewModel.updateAddress(address)
+                }
+            }
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+        )
     }
 
     private fun geoCodeLocation(location: Location): Address {
         val geocoder = Geocoder(context, Locale.getDefault())
         return geocoder.getFromLocation(location.latitude, location.longitude, 1)
-                .map { address ->
-                    Address(address.thoroughfare, address.subThoroughfare, address.locality, address.adminArea, address.postalCode)
-                }
-                .first()
+            .map { address ->
+                Address(
+                    address.thoroughfare,
+                    address.subThoroughfare,
+                    address.locality,
+                    address.adminArea,
+                    address.postalCode
+                )
+            }
+            .first()
     }
 
     private fun hideKeyboard() {
--- app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/representative/RepresentativeViewModel.kt
@@ -1,26 +1,37 @@
 package com.example.android.politicalpreparedness.representative
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.location.Geocoder
+import android.location.Location
+import androidx.lifecycle.*
+import com.example.android.politicalpreparedness.network.CivicsApi
+import com.example.android.politicalpreparedness.network.models.Address
+import com.example.android.politicalpreparedness.representative.model.Representative
+import kotlinx.coroutines.launch
+import java.util.*
 
-class RepresentativeViewModel: ViewModel() {
+class RepresentativeViewModel(application: Application): AndroidViewModel(application) {
+    private val _representatives = MutableLiveData<List<Representative>>()
+    val representatives: LiveData<List<Representative>> = _representatives
 
-    //TODO: Establish live data for representatives and address
+    private val _address = MutableLiveData<Address>()
+    val address: LiveData<Address> = _address
 
-    //TODO: Create function to fetch representatives from API from a provided address
+    init {
+        _address.value = Address("", "", "", "", "")
+    }
 
-    /**
-     *  The following code will prove helpful in constructing a representative from the API. This code combines the two nodes of the RepresentativeResponse into a single official :
+    fun getRepresentatives() {
+        viewModelScope.launch {
+            address.value?.let {
+                val (offices, officials) = CivicsApi.retrofitService.getRepresentatives(it.toFormattedString())
+                _representatives.value = offices.flatMap { office ->  office.getRepresentatives(officials)}
+            }
+        }
+    }
 
-    val (offices, officials) = getRepresentativesDeferred.await()
-    _representatives.value = offices.flatMap { office -> office.getRepresentatives(officials) }
-
-    Note: getRepresentatives in the above code represents the method used to fetch data from the API
-    Note: _representatives in the above code represents the established mutable live data housing representatives
-
-     */
-
-    //TODO: Create function get address from geo location
-
-    //TODO: Create function to get address from individual fields
-
+    fun updateAddress(address: Address){
+        _address.value = address
+        getRepresentatives()
+    }
 }
